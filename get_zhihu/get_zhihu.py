5926f13bad1b72b73f9994e4a41ef6d50a561ed6
Crossin Yuan <crossin@crossincode.com>
2018-07-23 18:18:23 +0800

    zhihu zhuanlan

---
+++ get_zhihu/get_zhihu.py
@@ -0,0 +1,86 @@
+import time
+import re
+import os
+import requests
+from bs4 import BeautifulSoup
+
+def get_list():
+    url = 'https://www.zhihu.com/api/v4/columns/%s/articles?include=data[*].topics&limit=10' % author
+    article_dict = {}
+    while True:
+        print('fetching', url)
+        try:
+            resp = requests.get(url, headers=headers)
+            j = resp.json()
+            data = j['data']
+        except:
+            print('get list failed')
+
+        for article in data:
+            aid = article['id']
+            akeys = article_dict.keys()
+            if aid not in akeys:
+                article_dict[aid] = article['title']
+
+        if j['paging']['is_end']:
+            break
+        url = j['paging']['next']
+        time.sleep(2)
+
+    with open('zhihu_ids.txt', 'w') as f:
+        items = sorted(article_dict.items())
+        for item in items:
+            f.write('%s %s\n' % item)
+
+def get_html(aid, title, index):
+    title = title.replace('/', '／')
+    title = title.replace('\\', '＼')
+    file_name = '%03d. %s.html' % (index, title)
+    if os.path.exists(file_name):
+        print(title, 'already exists.')
+        return
+    else:
+        print('saving', title)
+    url = 'https://zhuanlan.zhihu.com/p/' + aid
+    html = requests.get(url, headers=headers).text
+    print(html)
+    soup = BeautifulSoup(html, 'lxml')
+    content = soup.find(class_='Post-RichText').prettify()
+    content = content.replace('data-actual', '')
+    content = re.sub(r'<noscript>.*?</noscript>', '', content)
+    content = re.sub(r'src="data:image.*?"', '', content)
+    content = '<!DOCTYPE html><html><head><meta charset="utf-8"></head><body><h2>%s</h2>%s</body></html>' % (
+        title, content)
+    with open(file_name, 'w') as f:
+        f.write(content)
+    time.sleep(2)
+
+def get_details():
+    with open('zhihu_ids.txt') as f:
+        i = 1
+        for line in f:
+            lst = line.strip().split(' ')
+            aid = lst[0]
+            title = '_'.join(lst[1:])
+            get_html(aid, title, i)
+            i += 1
+
+def to_pdf():
+    import pdfkit
+    htmls = []
+    for root, dirs, files in os.walk('.'):
+        htmls += [name for name in files if name.endswith(".html")]
+    pdfkit.from_file(sorted(htmls), author + '.pdf')
+
+if __name__ == '__main__':
+    author = input('Please input author name:(default crossin)')
+    if not author:
+        author = 'crossin'
+    headers = {
+        'origin': 'https://zhuanlan.zhihu.com',
+        'referer': 'https://zhuanlan.zhihu.com/%s' % author,
+        'User-Agent': ('Mozilla/5.0'),
+    }
+    get_list()
+    get_details()
+    to_pdf()
---
+++ get_zhihu/readme.md
@@ -0,0 +1,21 @@
+需要安装库：requests、bs4、pdfkit（可选，导出pdf需要）
+
+
+
+抓取时需输入专栏url的路径，如我的专栏 https://zhuanlan.zhihu.com/crossin，需输入 **crossin**
+
+如需程序自动导出pdf，除通过pip安装pdfkit外，还需要手动安装 **wkhtmltopdf**，具体参见：
+
+https://github.com/JazzCore/python-pdfkit/wiki/Installing-wkhtmltopdf
+
+https://wkhtmltopdf.org/downloads.html
+
+输出示例：Crossin的编程教室-知乎专栏pdf版：https://pan.baidu.com/s/17Q7xvtsWJfF0GGBJXF8hSQ
+
+
+
+更多实用有趣的例程
+
+欢迎关注“**Crossin的编程教室**”及同名 [知乎专栏](https://zhuanlan.zhihu.com/crossin)
+
+![crossincode](../crossin-logo.png)
